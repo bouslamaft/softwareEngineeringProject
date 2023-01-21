b7760216f4eb49d9e782c2a74eccd837c386bd9d
fathi bouslama <bouslamafathi90@gmail.com>
2023-01-21 11:45:38 +0100

    fixing something

--- libraryManagemetSystem/libraryManagemetSystem/urls.py
+++ libraryManagemetSystem/libraryManagemetSystem/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , views.home  ) , 
+    path('' , views.home, name='home'  ) , 
 ]
