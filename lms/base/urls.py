5991ed7f8be60bd370e826860fd16bae946f5b6c
CreativePhilip <creativepyt@gmail.com>
2023-01-24 13:52:49 +0100

    feat: HUGE CHANGE refactor the messy codebase + minimize future merge conflicts

---
+++ lms/base/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls import include
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from lms.base.views.student import student_view
+from lms.base.views.librarian import librarian_view
+from lms.base.views.home import home_view
+from lms.base.views.about_us import about_us_view
+from lms.base.views.list_books import list_books_view
+
+
+urlpatterns = [
+    path('', home_view),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('listBooks/', list_books_view),
+
+    path('student/', student_view, name='student'),
+
+    path('librarian/', librarian_view, name='librarian'),
+
+    path('logout', LogoutView.as_view(template_name='library/index.html')),
+
+    path('aboutus', about_us_view),
+    # path('contactus', views.contactus_view),
+]
+
