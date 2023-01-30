a0f21cdb6469097f87975fb5d7a3506002263a32
fathi bouslama <bouslamafathi90@gmail.com>
2023-01-30 23:30:30 +0100

    studentInterface

--- lms/base/views/student.py
+++ lms/base/views/student.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from lms.storage.models import PhysicalBook
 
 
 def student_view(request):
-    return render(request, 'library/student.html')
+    rented_books = []
+    for pyh_books in PhysicalBook.objects.all():
+        if pyh_books.is_currently_rented and pyh_books.current_rent_history.rented_by == request.user:
+            rented_books.append(pyh_books)
+    
+    #print(rented_books)
+    context = { 'books': rented_books}
+
+
+    return render(request, 'library/student.html', context)
