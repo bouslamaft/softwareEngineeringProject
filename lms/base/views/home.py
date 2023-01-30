4c57c86c26d396aed432da3c35e1783b91b0f741
CreativePhilip <creativepyt@gmail.com>
2023-01-30 23:51:50 +0100

    feat: conditional navbar links + fixed crashing on homepage

--- lms/base/views/home.py
+++ lms/base/views/home.py
@@ -4,26 +4,21 @@ from django.shortcuts import render
 from lms.storage.models import Book, Category
 import random
 
+
 def home_view(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect('/login')
 
-    # return render(request, 'index.html')  # Fathi view
-    #if request.user.is_authenticated:
-    #    return HttpResponseRedirect('afterlogin')
-    # return render(request,'index.html')
-    
-    categories = Category.objects.all()
-    
-    rngVariable = random.randint(0, categories.count()-1)
-    
-    filterValue = categories[rngVariable]
-    
-    books = Book.objects.filter(categories=filterValue)
-    
-    print(filterValue)
-    print(books)
-    print(books.count())
-    
-    context = {'books_obj': books[1:4], 'book_ind':books[0], 'category': filterValue}
+    random_category = _random_category()
+    books_query = Book.objects.filter(categories=random_category)
+    books = list(books_query)
+
+    context = {'books_obj': books[1:4], 'book_ind': books[0], 'category': random_category}
     return render(request, 'index.html', context)  # Fathi view
+
+
+def _random_category() -> Category:
+    while True:
+        category = Category.objects.all().order_by("?").first()
+        if category.books.count() > 0:
+            return category
